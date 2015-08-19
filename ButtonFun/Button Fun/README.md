bb731d4cd3f70e7b8685b4b00225e2ac8b6e7e41
ujwala10 <ujwala.v10@gmail.com>
2015-08-19 15:09:30 -0400

    Project
    added ButtonFun Project

---
+++ ButtonFun/Button Fun/README.md
@@ -0,0 +1,17 @@
+ButtonFun
+=========
+
+### Requirements
+*	Develop a universal iOS app with no status bar with the name “Button Fun”.
+* Fill the screen with 40 px x 40 px random colored squares laid out in a grid.
+* When the user presses a square, change the color of the square to a new one.
+* Handle all device orientations 
+* Make the code as reusable as possible.
+* Bonus points, try not to use UIButtons
+
+### Setup and Submission Instructions
+
+* Clone this repository to your local machine.
+* Commit your changes to a new branch.
+* Push that branch back to Github.
+* When complete, create a pull request with the head branch as your new branch and the base branch as master.
---
+++ ButtonFun/Button Fun/ViewController.m
@@ -0,0 +1,94 @@
+//
+//  ViewController.m
+//  Button Fun
+//
+//  Created by Ujwalashish on 8/19/15.
+//  Copyright (c) 2015 Ujwalashish. All rights reserved.
+//
+
+#import "ViewController.h"
+
+
+static const CGSize SQUARE_SIZE = { 40, 40 };
+
+@interface ViewController ()
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    //add views
+    [self createAndAddSquares];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void) createAndAddSquares {
+    int w = self.view.frame.size.width;
+    int h = self.view.frame.size.height;
+    
+    for(int x = 0; x <= w; x+= 40) {
+        for(int y = 0; y <= h; y+= 40) {
+            CGPoint point = CGPointMake(x, y);
+            [self addSquare: point];
+        }
+    }
+}
+
+- (void) updateSquares {
+    while ([self.view.subviews count] > 0) {
+        UIView *v = [self.view.subviews objectAtIndex:0];
+        [v removeFromSuperview];
+    }
+    [self createAndAddSquares];
+}
+
+- (void) addSquare : (CGPoint) point {
+    CGRect frame;
+    frame.origin = point;
+    frame.size = SQUARE_SIZE;
+    UIView *squareView = [[UIView alloc] initWithFrame:frame];
+    squareView.backgroundColor = [self randomColor];
+    [self.view addSubview:squareView];
+    squareView.translatesAutoresizingMaskIntoConstraints = NO;
+}
+
+- (UIColor *) randomColor {
+    switch (arc4random()%5) {
+        case 0: return [UIColor blueColor];
+        case 1: return [UIColor greenColor];
+        case 2: return [UIColor orangeColor];
+        case 3: return [UIColor redColor];
+        case 4: return [UIColor purpleColor];
+    }
+    return [UIColor blackColor];
+}
+
+- (IBAction)tapGesture:(UITapGestureRecognizer*) sender {
+    if(sender.numberOfTapsRequired == 1) {
+        CGPoint location = [sender locationInView:self.view];
+        CALayer *hitLayer = [self.view.layer hitTest: location];
+        
+        //check if dat layer is one of the squareview
+        for(UIView *subview in self.view.subviews) {
+            if([subview isKindOfClass:[UIView class]])
+            {
+                if(hitLayer == subview.layer) {
+                    [subview setBackgroundColor:[UIColor whiteColor]];
+                }
+            }
+        }
+    }
+}
+
+-(void)updateViewConstraints {
+    [super updateViewConstraints];
+    [self updateSquares];
+}
+
+@end
